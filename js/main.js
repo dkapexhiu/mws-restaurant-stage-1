bc89d2f7a70e6ce12cf72e2c99ff24201d1c5853
Daniel <daniel.kapexhiu@gmail.com>
2018-03-04 22:16:59 +0100

    Update main.js after some bugfixes

--- js/main.js
+++ js/main.js
@@ -181,7 +181,7 @@ createRestaurantHTML = (restaurant) => {
     li.append(picture);
   }
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
